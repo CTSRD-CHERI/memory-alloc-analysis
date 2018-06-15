ef093a33bb92232e2c1d6de13f4d89b3939de6a8
Lucian Paul-Trifu <lmp53@cam.ac.uk>
2018-06-15 17:32:41 +0100

    Fix run-info output format
    At this point, there are two possible line formats:
    timestamp-unix-ns<tab>callstack<tab>callname<tab>ar gu ments<tab>result
    timestamp-unix-ns<tab>size-b

--- common/unrun.py
+++ common/unrun.py
@@ -12,23 +12,23 @@ class Unrun:
         # XXX: call stack not plumbed through yet
         # and we lose information about precisely which allocator call it was
         # (i.e. malloc vs. calloc vs. aligned_alloc vs. posix_memalign ....)
-        print("%d\t---\tmalloc\t%x\t%d" % (self._tslam(), begin, end - begin), file=self._out)
+        print("%d\t---\tmalloc\t%d\t%x" % (self._tslam(), end - begin, begin), file=self._out)
 
     def freed(self, publ, begin):
-        print("%d\t\tfree\t%x" % (self._tslam(), begin), file=self._out)
+        print("%d\t\tfree\t%x\t" % (self._tslam(), begin), file=self._out)
 
     def reallocd(self, publ, begin_old, begin_new, end_new):
         # XXX: call stack not plumbed through yet
-        print("%d\t---\trealloc\t%x\t%d\t%x" % (self._tslam(), begin_old, end_new - begin_new, begin_new), file=self._out)
+        print("%d\t---\trealloc\t%x %d\t%x" % (self._tslam(), begin_old, end_new - begin_new, begin_new), file=self._out)
 
     def mapd(self, publ, begin, end):
-        print("%d\t---\tmmap\t%x\t%d" % (self._tslam(), begin, end - begin))
+        print("%d\t---\tmmap\t0 %d\t%x" % (self._tslam(), end - begin, begin))
 
     def unmapd(self, publ, begin, end):
-        print("%d\t---\tmunmap\t%x\t%d" % (self._tslam(), begin, end - begin))
+        print("%d\t---\tmunmap\t%x %d\t" % (self._tslam(), begin, end - begin))
 
     def revoked(self, publ, spans):
-        print("%d\t\trevoke\t%s" % (self._tslam(), "\t".join(map(str,itertools.chain.from_iterable([[b,e] for (b,e) in spans])))))
+        print("%d\t\trevoke\t%s\t" % (self._tslam(), " ".join(map(str,itertools.chain.from_iterable([[b,e] for (b,e) in spans])))))
 
     def size_measured(self, publ, size):
         print("%d\t%d" % (self._tslam(), size))
