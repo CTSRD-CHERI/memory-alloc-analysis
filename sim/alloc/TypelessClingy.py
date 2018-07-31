798e213b85065328c39c2963c66fda4bb6e8e591
Nathaniel Filardo <nwf20@cl.cam.ac.uk>
2018-07-31 15:11:34 +0100

    TypelessClingy: fixes, tidying, add support for multiple revocations
    Adds --paranoia and --revoke-k parameters.  The former brings assertions
    in and the latter determines the number of revocations to run at once,
    selecting the k largest JUNK spans to TIDY.

--- sim/alloc/TypelessClingy.py
+++ sim/alloc/TypelessClingy.py
@@ -6,6 +6,7 @@
 # Preamble and global parameters -------------------------------------- {{{
 
 import argparse
+from bisect import insort
 from enum import Enum, unique
 import logging
 import sys
@@ -18,25 +19,34 @@ from sim.RenamingAllocatorBase import RenamingAllocatorBase
 bucklog = 16
 pagelog = 12
 
+# Various symbolic names for paranoia levels
+PARANOIA_STATE_ON_REVOKE=0
+PARANOIA_STATE_PER_OPER=1
+
 # --------------------------------------------------------------------- }}}
 # Bucket lifecycle definition and prose ------------------------------- {{{
 
 @unique
 class BuckSt(Enum):
-  TIDY = 1
-  BUMP = 2
-  WAIT = 3
-  JUNK = 4
+  AHWM = 1
+  TIDY = 2
+  BUMP = 3
+  WAIT = 4
+  JUNK = 5
   __repr__ = Enum.__str__
 
 # Bucket lifecycle:
 #
-#   All buckets initially are considered TIDY, meaning that there are
-#   certainly no pointers into this bucket.
+#   All buckets initially are considered AHWM, meaning that there are
+#   certainly no pointers into this bucket and that it has never been used.
+#   ("Above High Water Mark")
+#
+#   TIDY means that there are certainly no pointers into this bucket, but
+#   that it may have been used before.
 #
 #   When an allocation request arrives, there are three cases to consider:
 #
-#     If the allocation is "large", then we must find several TIDY
+#     If the allocation is "large", then we must find several TIDY or AHWM
 #     buckets to back it.  These buckets are immediately transitioned to
 #     WAIT state and are MAPPED.
 #
@@ -45,11 +55,11 @@ class BuckSt(Enum):
 #     If this fills the bucket, transition it to WAIT state, otherwise,
 #     leave it in BUMP state.
 #
-#     Otherwise, find a TIDY bucket and transition it to BUMP state,
+#     Otherwise, find a TIDY or AHWM bucket and transition it to BUMP state,
 #     setting its size, seeding its initial bitmap, and marking its pages
-#     as MAPPED.  TIDY buckets come from beyond our maximum allocated
-#     address or from a reused bucket below that address, based on some
-#     permitted "slop" threshold.
+#     as MAPPED.  AHWM buckets come from beyond our maximum allocated
+#     address and TIDY buckets from a reused bucket below that address, based
+#     on some permitted "slop" threshold.
 #
 #   When a free arrives, there are several cases as well:
 #
@@ -62,7 +72,7 @@ class BuckSt(Enum):
 #
 #   A free might also cause us to cross our "slop" threshold; if so, we
 #   will engage in one round of revocation of the largest contiguous block
-#   of JUNK|TIDY space, moving all blocks therein to TIDY.  (The
+#   of JUNK or TIDY space, moving all blocks therein to TIDY.  (The
 #   re-revocation of TIDY space may be surprising, but this reduces the
 #   pathology of interdigitated JUNK and TIDY causing many small
 #   revocations)
@@ -108,6 +118,8 @@ class Allocator(RenamingAllocatorBase):
         '_maxbix',
         '_njunkb',
         '_overhead_factor',
+        '_paranoia',
+        '_revoke_k',
         '_sz2bixp')
 
   def __init__(self, **kwargs):
@@ -120,8 +132,15 @@ class Allocator(RenamingAllocatorBase):
     argp.add_argument('--realloc', action='store',
                       type=str, default="always",
                       choices=['always', 'yes', 'onlyshrink', 'never', 'no'])
+    argp.add_argument('--paranoia', action='store', type=int, default=0)
+    argp.add_argument('--revoke-k', action='store', type=int, default=1)
     args = argp.parse_args(kwargs['cliargs'])
+
     self._overhead_factor = args.overhead_factor
+    self._paranoia        = args.paranoia
+
+    assert args.revoke_k > 0
+    self._revoke_k        = args.revoke_k
 
     if args.realloc == "never" or args.realloc == "no" :
         self._try_realloc = self._try_realloc_never
@@ -136,7 +155,7 @@ class Allocator(RenamingAllocatorBase):
     self._sz2bixp = {}  # BUMP buckets and allocation pointer, by size
     self._bix2szbm = {} # BUMP and WAIT buckets' size and bitmaps
     self._njunkb = 0    # Number of buckets in JUNK state
-    self._bix2state = IntervalMap(0, 2**(64 - bucklog), BuckSt.TIDY)
+    self._bix2state = IntervalMap(0, 2**(64 - bucklog), BuckSt.AHWM)
 # --------------------------------------------------------------------- }}}
 # Additional state assertions and diagnostics ------------------------- {{{
 
@@ -149,7 +168,7 @@ class Allocator(RenamingAllocatorBase):
     # Ensure that our _maxbix looks like the HWM
     (mbase, msz, mv) = self._bix2state[self._maxbix]
     assert mbase + msz == 2**(64 - bucklog), ("maxbix not max", self._maxbix, mbase, msz, mv)
-    assert mv == BuckSt.TIDY, ("maxbix not tidy", self._maxbix, mbase, msz, mv)
+    assert mv == BuckSt.AHWM, ("maxbix not AHWM", self._maxbix, mbase, msz, mv)
 
     # Check that our running sum of JUNK pages is correct
     js = [sz for (_, sz, v) in self._bix2state if v == BuckSt.JUNK]
@@ -159,20 +178,21 @@ class Allocator(RenamingAllocatorBase):
     for b in [(l,sz) for (l,sz,v) in self._bix2state if v == BuckSt.BUMP] :
         for bc in range(b[0],b[0]+b[1]):
             (bsz, _) = self._bix2szbm[bc]
-            assert self._sz2bixp.get(bsz)[0] == bc, \
-                ("BUMP miss", bc, bsz, self._state_diag())
+            assert self._sz2bixp.get(bsz) is not None, \
+                ("BUMP miss ix", bc, bsz, self._state_diag())
+            assert self._sz2bixp[bsz][0] == bc, \
+                ("BUMP miss eq", bc, bsz, self._state_diag())
 
     # Same for WAIT states.  Not all WAIT-state buckets are necessarily indexed,
-    # tho', so we have to be somewhat careful 
+    # tho', so we have to be somewhat careful
     for b in [(l,sz) for (l,sz,v) in self._bix2state if v == BuckSt.WAIT] :
         bc = b[0]
         bce = b[0] + b[1]
-        while bc <= bce:
+        while bc < bce:
             assert self._bix2szbm.get(bc) is not None, \
                 ("B/W miss", bc, self._state_diag())
             (bsz, _) = self._bix2szbm[bc]
-            bc += bsz
-
+            bc += (bsz + 2**bucklog - 1) >> bucklog
 
     # Every currently-active BUMP bucket is tagged as such, yes?
     for sz in self._sz2bixp :
@@ -191,11 +211,11 @@ class Allocator(RenamingAllocatorBase):
 
   # An actual implementation would maintain a prioqueue or something;
   # we can get away with a linear scan.
-  def _find_largest_revokable_span(self):
+  def _find_largest_revokable_spans(self, n=1):
+    # Exclude AHWM, which is like TIDY but would almost always be biggest
     okst = {BuckSt.TIDY, BuckSt.JUNK}
-    bestbix = None
-    bestsz = -1
-    bestnj = None
+
+    bests = [(0, (None, None))] # [(sz, (bix, njunk))] in ascending order
     cursorbix = 0
     while cursorbix < self._maxbix :
         (qbase, qsz, qv) = self._bix2state.get(cursorbix,
@@ -205,47 +225,48 @@ class Allocator(RenamingAllocatorBase):
         cursorbix += qsz
 
         # Smaller or busy spans don't interest us
-        if qsz <= bestsz : continue
+        if qsz <= bests[0][0] : continue
         if qv not in okst : continue
 
         # Reject spans that are entirely TIDY already.
-        # Avoid the expensive slice if we already know the answer.
         js = [sz for (_, sz, v) in self._bix2state[qbase:qbase+qsz]
                   if v == BuckSt.JUNK]
         if js == [] : continue
-        bestnj = sum(js)
-        bestsz = qsz
-        bestbix = qbase
-    
-    # Not strictly necessary, just tidy the return value by avoiding
-    # the certainly-TIDY spans past _maxbix
-    if bestbix is not None and bestbix + bestsz >= self._maxbix :
-        bestsz = self._maxbix - bestbix
 
-    return (bestbix, bestsz, bestnj)
+        insort(bests, (qsz, (qbase, sum(js))))
+        bests = bests[(-n):]
+
+    return bests
 
   def _try_revoke(self) :
     nbusy = len(self._bix2szbm)
     if self._maxbix >= self._overhead_factor * nbusy :
-        self._state_asserts() # Periodically check that we've gotten it right
-        brs = self._find_largest_revokable_span()
-        if brs is not None :
+
+        # Periodically check that we've gotten it right
+        if self._paranoia > PARANOIA_STATE_ON_REVOKE : self._state_asserts()
+
+        brss = self._find_largest_revokable_spans(n=self._revoke_k)
+        brss = [ (bix, sz, nj) for (sz, (bix, nj)) in brss if sz > 0 ]
+        for brs in brss :
             self._njunkb -= brs[2]
             self._bix2state.mark(brs[0],brs[1],BuckSt.TIDY)
-            self._publish('revoked', [(brs[0] << bucklog, \
-                                      (brs[0]+brs[1]) << bucklog)])
+
+        brss = [(bix << bucklog, (bix+sz) << bucklog) for (bix, sz, _) in brss]
+        self._publish('revoked', brss)
 
 # --------------------------------------------------------------------- }}}
 # Allocation ---------------------------------------------------------- {{{
+
+  # Find a TIDY|AHWM location for reqbsz buckets and transition them into
+  # newst
   def _locnew(self, reqbsz, nst):
     # A linear search isn't too terrible, I suppose
     for (base, sz, val) in self._bix2state :
-      if val != BuckSt.TIDY : continue
+      if val not in { BuckSt.TIDY, BuckSt.AHWM } : continue
       if sz < reqbsz : continue
       if base >= self._maxbix : continue
-      
-      if base + reqbsz >= self._maxbix :
-        self._maxbix += base + reqbsz
+
+      self._maxbix = max(self._maxbix, base + reqbsz)
 
       self._bix2state.mark(base, reqbsz, nst)
       return base
@@ -254,9 +275,11 @@ class Allocator(RenamingAllocatorBase):
     self._maxbix += reqbsz
     self._bix2state.mark(loc, reqbsz, nst)
     return loc
- 
+
   def _alloc(self, sz):
     logging.debug(">_alloc sz=%d", sz)
+    if self._paranoia > PARANOIA_STATE_PER_OPER : self._state_asserts()
+
     if _issmall(sz) :
       # Is small allocation
 
@@ -304,8 +327,11 @@ class Allocator(RenamingAllocatorBase):
 
 # --------------------------------------------------------------------- }}}
 # Free ---------------------------------------------------------------- {{{
+
   def _free(self, eva) :
     logging.debug(">_free eva=%x", eva)
+    if self._paranoia > PARANOIA_STATE_PER_OPER : self._state_asserts()
+
     # Look up existing allocation
     bix = eva >> bucklog
     b   = self._bix2szbm[bix]
@@ -324,12 +350,21 @@ class Allocator(RenamingAllocatorBase):
       bitm  = 1 << bitix
       assert bbm & bitm == 0, "Free of bitmask-free object"
       bbm |= bitm
+
+      if spanst == BuckSt.BUMP :
+        (bbix, bbap) = self._sz2bixp[sz]
+        assert bbix == bix, ("Free in a BUMP bucket but not the BUMP bucket", \
+            sz, bix, bbix)
+        assert bitix < bbap, ("Free in BUMP bucket beyond alloc ptr", \
+            sz, bix, bitix, bbap)
+
       if bbm == (1 << _maxoix(sz)) - 1 :
         # All objects now free; move bucket state
-        del self._bix2szbm[bix]
-        assert self._sz2bixp.get(sz) is None or self._sz2bixp[sz][1] != bix, \
-          "Freeing bucket still registered as bump block"
+        assert self._sz2bixp.get(sz) is None or self._sz2bixp[sz][0] != bix, \
+          ("Freeing bucket still registered as bump block", \
+            bix, sz, self._bix2szbm[bix], self._sz2bixp.get(sz))
         assert spanst == BuckSt.WAIT, "Freeing bucket in non-WAIT state"
+        del self._bix2szbm[bix]
         self._bix2state.mark(bix, 1, BuckSt.JUNK)
         self._njunkb += 1
 
@@ -347,7 +382,8 @@ class Allocator(RenamingAllocatorBase):
       bsz = int((sz + 2**bucklog - 1) >> bucklog)
       del self._bix2szbm[bix]
 
-      assert spanst == BuckSt.WAIT
+      assert spanst == BuckSt.WAIT, \
+        ("Freeing large span in incorrect state", sz, spanst, bix, b, self._state_diag())
       assert bix + bsz <= spanbase + spansize, \
         "Mismatched bucket states of large allocation"
 
@@ -370,6 +406,8 @@ class Allocator(RenamingAllocatorBase):
   # If you're not convinced by the above, you're exactly the kind of person
   # that --realloc=onlyshrink or --realloc=none are for!
   def _try_realloc_yes(self, oeva, nsz):
+    if self._paranoia > PARANOIA_STATE_PER_OPER : self._state_asserts()
+
     # Find the size of the existing allocation
     bix = oeva >> bucklog
     b   = self._bix2szbm[bix]
@@ -382,40 +420,48 @@ class Allocator(RenamingAllocatorBase):
     (osz, _) = b
 
     if nsz <= osz :
+      logging.debug("<_try_realloc shrink eva=%x", oeva)
       # Shrinking is always fine, I suppose
       # Don't update the block size
       return True
 
-    if _issmall(nsz) :
+    if _issmall(osz) :
+      logging.debug("<_try_realloc small eva=%x", oeva)
       # Small allocation not growing by much.
-      return _szfix(nsz) == osz 
+      return _issmall(nsz) and _szfix(nsz) == osz
       # Unfortunately, even if the next small piece is free, we couldn't
       # use it, because we'd then be waiting for a free() that never came
 
     # Large allocation getting larger.  If not much larger...
     if nsz <= ((osz + 2**bucklog - 1) >> bucklog) << bucklog :
-        self._bix2szbm[bix] = (nsz, 0)
-        return True
+      logging.debug("<_try_realloc sm enlarging eva=%x (bix=%d) osz=%d nsz=%d %s", \
+          bix << bucklog, bix, osz, nsz, self._state_diag())
+      self._bix2szbm[bix] = (nsz, 0)
+      return True
 
     # It might happen that we have enough free spans ahead of us that we can
     # just gobble them.
     (nextbase, nextsize, nextst) = self._bix2state.get(bix+(osz >> bucklog))
-    if nextst != BuckSt.TIDY : return False
+    if nextst not in { BuckSt.TIDY, BuckSt.AHWM } :
+      logging.debug("<_try_realloc up against %s at eva=%x", nextst, oeva)
+      return False
 
     if nsz <= osz + (nextsize << bucklog) :
-        logging.debug("=_try_realloc enlarging eva=%x osz=%d nsz=%d", \
-            bix << bucklog, osz, nsz)
-        self._bix2szbm[bix] = (nsz, 0)
-        self._bix2state.mark(bix+(osz >> bucklog),
-                             (nsz - osz + 2**bucklog - 1) >> bucklog,
-                             BuckSt.WAIT)
-        self._publish('mapd', (bix+(osz >> bucklog)) << bucklog, \
-                    (bix + ((nsz + 2 **bucklog -1) >> bucklog)) << bucklog)
-        return True
+      logging.debug("<_try_realloc enlarging eva=%x osz=%d nsz=%d", \
+          bix << bucklog, osz, nsz)
+      self._bix2szbm[bix] = (nsz, 0)
+      self._bix2state.mark(bix+(osz >> bucklog),
+                           (nsz - osz + 2**bucklog - 1) >> bucklog,
+                           BuckSt.WAIT)
+      self._publish('mapd', (bix+(osz >> bucklog)) << bucklog, \
+                  (bix + ((nsz + 2 **bucklog -1) >> bucklog)) << bucklog)
+      return True
 
     return False
 
   def _try_realloc_onlyshrink(self, oeva, nsz):
+    if self._paranoia > PARANOIA_STATE_PER_OPER : self._state_asserts()
+
     # Find the size of the existing allocation
     bix = oeva >> bucklog
     b   = self._bix2szbm[bix]
@@ -427,7 +473,11 @@ class Allocator(RenamingAllocatorBase):
     return nsz <= osz
 
   def _try_realloc_never(self, oeva, nsz):
+    # Don't bother with PARANOIA_STATE_PER_OPER since we're just going to
+    # call down anyway
+
     return False
+
 # --------------------------------------------------------------------- }}}
 
 # vim: set foldmethod=marker:foldmarker={{{,}}}
