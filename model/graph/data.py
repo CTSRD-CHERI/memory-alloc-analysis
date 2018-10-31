5e2a93f78cbd70b2f1aa647d42a5deff58276838
Lucian Paul-Trifu <lmp53@cam.ac.uk>
2018-10-31 15:24:15 +0000

    model/graph: Cosmetic changes

--- model/graph/data.py
+++ model/graph/data.py
@@ -1,5 +1,7 @@
 #import glob
 import sys
 
-data0 = [[int(s.strip()) for s in line.split('\t')] for line in sys.stdin if not line.startswith('#')]
-data0_label = 'Chromium'
+fn = sys.argv[1] if len(sys.argv) > 1 else None
+with open(fn, 'r') if fn is not None else sys.stdin as f:
+    data0 = [[int(s.strip()) for s in line.split('\t')] for line in f if not line.startswith('#')]
+data0_label = fn if fn is not None else ''
--- model/graph/graph.py
+++ model/graph/graph.py
@@ -31,20 +31,19 @@ plt.subplot(4, 1, 1)
 plt.subplots_adjust(hspace=1)
 
 plt.plot(time_s, data0[y1], time_s, data0[y2], time_s, data0[y3], time_s, data0[y4])
-plt.title('{0} allocator address space usage over time'.format(data0_label))
+plt.title('Address space usage over time\n(data-set "{0}")'.format(data0_label))
 plt.legend(['Aspace total', 'Aspace to sweep', 'Aspace of allocator', 'Allocd by allocator'],
            loc='lower right', bbox_to_anchor=[1, 1])
 plt.xlabel('Time (s)')
 plt.ylabel('Amount (mb)')
-plt.savefig('{0}-aspace_stats-vs-time.eps'.format(data0_label.lower()))
 
 plt.subplot(4, 1, 2)
 sweep_per_ns = (data0[y6][1:] - data0[y6][:-1]) / (data0[x][1:] - data0[x][:-1])
 sweep_per_s = sweep_per_ns * 10**9
 #print(sweep_per_s)
 plt.plot(time_s[:-1], sweep_per_s, color='blue')
-plt.title('{0} sweeping amount requirement over time\nmax={1}gb/s   avg={2}gb/s'
-          .format(data0_label, int(max(sweep_per_s)), int(np.average(sweep_per_s))))
+plt.title('Sweeping amount requirement over time\nmax={0}gb/s   avg={1}gb/s'
+          .format(int(max(sweep_per_s)), int(np.average(sweep_per_s))))
 plt.xlabel('Time (s)')
 plt.ylabel('Amount (gb/s)')
 
@@ -52,8 +51,8 @@ plt.subplot(4, 1, 3)
 sweeps_per_ns = (data0[y5][1:] - data0[y5][:-1]) / (data0[x][1:] - data0[x][:-1])
 sweeps_per_s = sweeps_per_ns * 10**9
 plt.plot(time_s[:-1], sweeps_per_s, color='red')
-plt.title('{0} sweeps required over time\nmax={1}k/s   avg={2}k/s'
-          .format(data0_label, int(max(sweeps_per_s)), int(np.average(sweeps_per_s))))
+plt.title('Sweeps required over time\nmax={0}k/s   avg={1}k/s'
+          .format(int(max(sweeps_per_s)), int(np.average(sweeps_per_s))))
 plt.xlabel('Time (s)')
 plt.ylabel('Amount (thousands)', color='red')
 
@@ -61,10 +60,10 @@ plt.subplot(4, 1, 4)
 sweeps_ivals_per_ns = (data0[y7][1:] - data0[y7][:-1]) / (data0[x][1:] - data0[x][:-1])
 sweeps_ivals_per_s = sweeps_ivals_per_ns * 10**9
 plt.plot(time_s[:-1], sweeps_ivals_per_s, color='blue')
-plt.title('{0} sweep intervals required over time\nmax={1}k/s   avg={2}k/s'
-          .format(data0_label, int(max(sweeps_ivals_per_s)), int(np.average(sweeps_ivals_per_s))))
+plt.title('Sweep intervals required over time\nmax={0}k/s   avg={1}k/s'
+          .format(int(max(sweeps_ivals_per_s)), int(np.average(sweeps_ivals_per_s))))
 plt.xlabel('Time (s)')
 plt.ylabel('Amount (thousands)', color='blue')
-plt.savefig('{0}-aspace_stats-vs-time.eps'.format(data0_label.lower()))
+plt.savefig('{0}-aspace_stats.eps'.format(data0_label.lower()))
 
 plt.show()
