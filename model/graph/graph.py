b6227053dc33f1399041c616d0164dbce863942e
Lucian Paul-Trifu <lmp53@cam.ac.uk>
2018-11-12 15:41:55 +0000

    model/graph: Use time windows for /s measures

--- model/graph/graph.py
+++ model/graph/graph.py
@@ -25,6 +25,17 @@ data0[y7] //= 1000         # sweep ivals units to thousands
 
 #print(data0)
 
+def cum_time_window(vals, time, win_size):
+    vals_ret = []
+    time_ret = []
+    win_start_i = 0
+    for t, v in zip(time, vals):
+        if t - time[win_start_i + 1] > win_size:
+            win_start_i += 1
+        vals_ret.append(v - vals[win_start_i])
+        time_ret.append(t - time[win_start_i])
+    return np.array(vals_ret), np.array(time_ret)
+
 matplotlib.rc('font', size=11)
 #fig, ax = plt.subplots()
 plt.subplot(4, 1, 1)
@@ -38,28 +49,29 @@ plt.xlabel('Time (s)')
 plt.ylabel('Amount (mb)')
 
 plt.subplot(4, 1, 2)
-sweep_per_ns = (data0[y6][1:] - data0[y6][:-1]) / (data0[x][1:] - data0[x][:-1])
-sweep_per_s = sweep_per_ns * 10**9
+sweep_1s, time_ns_windows = cum_time_window(data0[y6], data0[x], 10**9)
+time_s_windows = np.array([1 if tw < 10**9 else tw / 10**9 for tw in time_ns_windows])
+sweep_per_s = sweep_1s / time_s_windows
 #print(sweep_per_s)
-plt.plot(time_s[:-1], sweep_per_s, color='blue')
+plt.plot(time_s, sweep_per_s, color='blue')
 plt.title('Sweeping amount requirement over time\nmax={0}gb/s   avg={1}gb/s'
           .format(int(max(sweep_per_s)), int(np.average(sweep_per_s))))
 plt.xlabel('Time (s)')
 plt.ylabel('Amount (gb/s)')
 
 plt.subplot(4, 1, 3)
-sweeps_per_ns = (data0[y5][1:] - data0[y5][:-1]) / (data0[x][1:] - data0[x][:-1])
-sweeps_per_s = sweeps_per_ns * 10**9
-plt.plot(time_s[:-1], sweeps_per_s, color='red')
+sweeps_1s, _ = cum_time_window(data0[y5], data0[x], 10**9)
+sweeps_per_s = sweeps_1s / time_s_windows
+plt.plot(time_s, sweeps_per_s, color='red')
 plt.title('Sweeps required over time\nmax={0}k/s   avg={1}k/s'
           .format(int(max(sweeps_per_s)), int(np.average(sweeps_per_s))))
 plt.xlabel('Time (s)')
 plt.ylabel('Amount (thousands)', color='red')
 
 plt.subplot(4, 1, 4)
-sweeps_ivals_per_ns = (data0[y7][1:] - data0[y7][:-1]) / (data0[x][1:] - data0[x][:-1])
-sweeps_ivals_per_s = sweeps_ivals_per_ns * 10**9
-plt.plot(time_s[:-1], sweeps_ivals_per_s, color='blue')
+sweeps_ivals_1s, _ = cum_time_window(data0[y7], data0[x], 10**9)
+sweeps_ivals_per_s = sweeps_ivals_1s / time_s_windows
+plt.plot(time_s, sweeps_ivals_per_s, color='blue')
 plt.title('Sweep intervals required over time\nmax={0}k/s   avg={1}k/s'
           .format(int(max(sweeps_ivals_per_s)), int(np.average(sweeps_ivals_per_s))))
 plt.xlabel('Time (s)')
